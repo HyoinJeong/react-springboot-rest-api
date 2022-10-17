e72bff9b78d0545dac00478109346fedf19642f2
HyoinJeong <hiamy0107@naver.com>
2022-10-18 02:43:32 +0900

    feat: 게시판의 DB 접근 로직 구현

--- gc-coffee/src/main/java/com/example/gccoffee/model/Article.java
+++ gc-coffee/src/main/java/com/example/gccoffee/model/Article.java
@@ -1,20 +1,63 @@
 package com.example.gccoffee.model;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Index;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "type"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
-    private Long id;
-    private String title; // 제목
-    private String content; // 본문
-    private String type; // 분류
 
-    private LocalDateTime createdAt;
-    private String createdBy; // 생성자
-    private LocalDateTime updatedAt;
-    private String updatedBy;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false) private String title; // 제목
+    @Setter @Column(nullable = false, length = 10000) private String content; // 본문
+
+    @Setter private String type; // 분류
+
+    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100) private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime updatedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String updatedBy;
+
+    protected Article() {}
+
+    private Article(String title, String content, String type) {
+        this.title = title;
+        this.content = content;
+        this.type = type;
+    }
+
+    public static Article of(String title, String content, String type) {
+        return new Article(title, content, type);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
