6222dc3c5647f56da4805f2d7d28eb278c6a82ac
HyoinJeong <hiamy0107@naver.com>
2022-10-19 06:15:57 +0900

    feat: Spring Security 설정 및 수정

---
+++ gc-coffee/src/main/java/com/example/gccoffee/configuration/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.example.gccoffee.configuration;
+
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .mvcMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin().and()
+                .logout()
+                        .logoutSuccessUrl("/")
+                        .and()
+                .build();
+    }
+
+}
