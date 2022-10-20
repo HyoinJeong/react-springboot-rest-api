2dc71bd9223ae8933205727ed0d5245bdb5bfb8b
HyoinJeong <hiamy0107@naver.com>
2022-10-21 05:33:09 +0900

    feat: 댓글 DTO와 Service 로직 설정

---
+++ gc-coffee/src/main/java/com/example/gccoffee/service/ArticleCommentService.java
@@ -0,0 +1,28 @@
+package com.example.gccoffee.service;
+
+import com.example.gccoffee.dto.ArticleCommentDto;
+import com.example.gccoffee.repository.ArticleCommentRepository;
+import com.example.gccoffee.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+}
