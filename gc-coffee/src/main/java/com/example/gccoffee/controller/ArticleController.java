7896c8bd250fadd913ed7a147f08e92903cf81b9
HyoinJeong <hiamy0107@naver.com>
2022-10-21 05:18:21 +0900

    feat: 게시글 상세 보기 기능 로직 추가

--- gc-coffee/src/main/java/com/example/gccoffee/controller/ArticleController.java
+++ gc-coffee/src/main/java/com/example/gccoffee/controller/ArticleController.java
@@ -17,4 +17,12 @@ public class ArticleController {
         map.addAttribute("articles", List.of());
         return "articles/index";
     }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
+        map.addAttribute("articleComments", List.of());
+
+        return "articles/detail";
+    }
 }
