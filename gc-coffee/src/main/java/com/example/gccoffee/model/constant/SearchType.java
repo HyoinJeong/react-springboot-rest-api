0fcd44ac821f11d48d847802449d8e0be6074460
HyoinJeong <hiamy0107@naver.com>
2022-10-21 05:17:03 +0900

    feat: 게시판 서비스 로직 생성
    CRUD 기능

---
+++ gc-coffee/src/main/java/com/example/gccoffee/model/constant/SearchType.java
@@ -0,0 +1,5 @@
+package com.example.gccoffee.model.constant;
+
+public enum SearchType {
+    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+}
---
+++ gc-coffee/src/main/java/com/example/gccoffee/service/ArticleService.java
@@ -0,0 +1,40 @@
+package com.example.gccoffee.service;
+
+import com.example.gccoffee.dto.ArticleDto;
+import com.example.gccoffee.dto.ArticleUpdateDto;
+import com.example.gccoffee.model.constant.SearchType;
+import com.example.gccoffee.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleDto searchArticle(long l) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+}
